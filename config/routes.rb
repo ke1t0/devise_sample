faf1863859fe755e40f28cf661313b329e110f27
user_k <kei.tennis1413@icloud.com>
2020-02-15 23:06:12 +0900

    ツイート機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   # get 'home/index'
   # get 'home/show'
   root 'home#index'
   devise_for :users
-  resources :users, only: [:index, :show]
+  resources :users
+  resources :tweets
 end
