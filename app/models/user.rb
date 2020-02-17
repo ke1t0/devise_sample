661f94250a186842795aa53711e1b323d6c1792c
user_k <kei.tennis1413@icloud.com>
2020-02-17 18:22:51 +0900

    ファボ機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
-  has_many :tweets
+  has_many :tweets, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_tweets, through: :favorites, source: :tweet
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-
   # get 'home/index'
   # get 'home/show'
   root 'home#index'
   devise_for :users
   resources :users
-  resources :tweets
+  resources :tweets do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
